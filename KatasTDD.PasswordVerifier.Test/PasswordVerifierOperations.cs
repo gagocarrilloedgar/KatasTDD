d96ad83fcf502b5b78ae5aa821fccce22dfe51e1
Edgar Gago Carrillo <34156343+gagocarrilloedgar@users.noreply.github.com>
2018-07-25 10:33:04 +0200

    Add files via upload

---
+++ KatasTDD.PasswordVerifier.Test/PasswordVerifierOperations.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KatasTDD.PasswordVerifier.Library.Interface;
+using KatasTDD.PasswordVerifier.Library.Services;
+
+namespace KatasTDD.PasswordVerifier.Test
+{
+    [TestClass]
+    public class PasswordVerifierOperations
+    {
+        #region Properties
+        private IPasswordVerfierServices passwordVerifierServices;
+        #endregion
+
+        [TestInitialize]
+        public void Setup()
+        {
+            passwordVerifierServices = new PasswordVerifierServices();
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PasswordShorterThan8_ReturnsFalse()
+        {
+            //Arrange --> Setup()
+
+            Verify("hola");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void PasswordNull_ThrowsNullReferenceExcption()
+        {
+            Verify(null);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PasswordWithoutCaptialLetter_ThrowsException()
+        {
+            Verify("holamundo");
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PasswordNotConainsANumber_ThrowsException()
+        {
+            Verify("holamundo");
+
+        }
+
+        [TestMethod]
+        public void Verify_AtLEast3AreCorrect_ReturnsTrue()
+        {
+            var expected = Verify("Holamundo8");
+            var actual = true;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        #region ExtraMethods
+        private bool Verify(string password)
+        {   
+            //Act
+            var expected = passwordVerifierServices.Verify(password);
+
+            return expected;
+        }
+
+        #endregion
+
+
+    }
+}
+
+
+
+
+
---
+++ KatasTDD.PasswordVerifier.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KatasTDD.PasswordVerifier.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KatasTDD.PasswordVerifier.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5eaefa84-0223-4db5-b455-a03007c344a7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
